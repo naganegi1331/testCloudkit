29289071f2a27d3a661b752ccd8d857869190599
Hiroki Kashihara <himawarisun1331@gmail.com>
2025-04-19 20:18:01 +0900

    Initial Commit

---
+++ TestCloudKit/TestCloudKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCloudKitApp.swift
+//  TestCloudKit
+//
+//  Created by Hiroki Kashihara on 2025/04/19.
+//
+
+import SwiftUI
+
+@main
+struct TestCloudKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
